8cdf14a10c40720c9c4c00f9211cbe9de8a1eb3f
aisdev <aisosamatthew247@gmail.com>
2024-10-08 03:23:28 +0100

    css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["*.html"],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        text: 'hsl(247, 75%, 5%)',
+        background: 'hsl(240, 60%, 99%)',
+        primary: 'hsl(243, 68%, 48%)',
+        secondary: 'hsl(243, 100%, 93%)',
+        accent: 'hsl(242, 100%, 62%)'
+      }
+    },
   },
   plugins: [],
 }
