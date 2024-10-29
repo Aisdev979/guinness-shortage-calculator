2b4763a39daa134dcc29c64a0732258991ecaf95
aisdev <aisosamatthew247@gmail.com>
2024-10-29 03:24:14 +0100

    Added the funtionality for the different categories

--- script/script.js
+++ script/script.js
@@ -1,25 +1,74 @@
-console.log("hello");
-const numberOfItems = document.querySelector('#number_of_items');
-const wob = document.querySelector('#wob');
-const goodPack = document.querySelector('#good_pack');
-const goodCan = document.querySelector('#good_can');
-const badPack = document.querySelector('#bad_pack');
-const badCan = document.querySelector('#bad_can');
-const notReturnedOrMissing = document.querySelector('#not_return_or_missing');
-const cal = document.querySelector('#cal');
-const finalResult = document.querySelector('#result');
+(function() {
+    console.log("hello");
+    const twentyFourCat = document.querySelector('#twentyfour_categories')
+    const eighteenCat = document.querySelector('#eighteen_categories');
+    const twelveCat = document.querySelector('#twelve_categories');
+    const numberOfItems = document.querySelector('#number_of_items');
+    const wob = document.querySelector('#wob');
+    const goodPack = document.querySelector('#good_pack');
+    const goodCan = document.querySelector('#good_can');
+    const badPack = document.querySelector('#bad_pack');
+    const badCan = document.querySelector('#bad_can');
+    const notReturnedOrMissing = document.querySelector('#not_return_or_missing');
+    const cal = document.querySelector('#cal');
+    const finalResult = document.querySelector('#result');
 
-function text() {
-    let remainingPacks = +numberOfItems.value - +wob.value - +goodPack.value - +badPack.value - +notReturnedOrMissing.value;
-    let canConvertion = remainingPacks * 24;
-    let remainingCans = canConvertion - +goodCan.value - +badCan.value
-    let finalPack = Math.floor(remainingCans / 24);
-    let finalCan = remainingCans - finalPack * 24;
+    console.log(twentyFourCat, twelveCat, eighteenCat)
+    const twentyFourCategories = () => {
+        twentyFourCat.addEventListener('change', () => {
+            let twentyFourValue = twentyFourCat.value
+            console.log(twentyFourValue)
+            let remainingPacks = +numberOfItems.value - +wob.value - +goodPack.value - +badPack.value - +notReturnedOrMissing.value;
+            let canConvertion = remainingPacks * 24;
+            let remainingCans = canConvertion - +goodCan.value - +badCan.value
+            let finalPack = Math.floor(remainingCans / 24);
+            let finalCan = remainingCans - finalPack * 24;
+    
+            finalResult.textContent = `wob: ${+wob.value}, packs: ${finalPack}, cans: ${finalCan}`
+    
+            console.log(remainingPacks, canConvertion, badCan, goodCan, remainingCans, finalPack, finalCan)
+        })
+    }
 
-    finalResult.textContent = `wob: ${+wob.value}, packs: ${finalPack}, cans: ${finalCan}`
+    const eighteenCategories = () => {
+        eighteenCat.addEventListener('change', () => {
+            let eighteenValue = eighteenCat.value
+            console.log(eighteenValue)
+            let remainingPacks = +numberOfItems.value - +wob.value - +goodPack.value - +badPack.value - +notReturnedOrMissing.value;
+            let canConvertion = remainingPacks * 18;
+            let remainingCans = canConvertion - +goodCan.value - +badCan.value
+            let finalPack = Math.floor(remainingCans / 18);
+            let finalCan = remainingCans - finalPack * 18;
+    
+            finalResult.textContent = `wob: ${+wob.value}, packs: ${finalPack}, cans: ${finalCan}`
+    
+            console.log(remainingPacks, canConvertion, badCan, goodCan, remainingCans, finalPack, finalCan)
+        })
+    }
 
-    console.log(remainingPacks, canConvertion, badCan, goodCan, remainingCans, finalPack, finalCan)
-}
+    const twelveCategories = () => {
+        twelveCat.addEventListener('change', () => {
+            let twelveValue = twelveCat.value
+            console.log(twelveValue)
+            let remainingPacks = +numberOfItems.value - +wob.value - +goodPack.value - +badPack.value - +notReturnedOrMissing.value;
+            let canConvertion = remainingPacks * 12;
+            let remainingCans = canConvertion - +goodCan.value - +badCan.value
+            let finalPack = Math.floor(remainingCans / 12);
+            let finalCan = remainingCans - finalPack * 12;
+    
+            finalResult.textContent = `wob: ${+wob.value}, packs: ${finalPack}, cans: ${finalCan}`
+    
+            console.log(remainingPacks, canConvertion, badCan, goodCan, remainingCans, finalPack, finalCan)
+        })
+    }
 
-cal.addEventListener('click', text)
+    function text() {
+        twelveCategories()
+        twentyFourCategories()
+        eighteenCategories()
+    }
 
+    cal.addEventListener('click', text)
+
+
+})()
