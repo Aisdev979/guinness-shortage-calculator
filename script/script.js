74c3ef54315bf9668dd7ee398e928a8ce361e446
aisdev <aisosamatthew247@gmail.com>
2024-10-29 05:06:30 +0100

    Fixed the bug on the functionality

--- script/script.js
+++ script/script.js
@@ -1,5 +1,4 @@
 (function() {
-    console.log("hello");
     const twentyFourCat = document.querySelector('#twentyfour_categories')
     const eighteenCat = document.querySelector('#eighteen_categories');
     const twelveCat = document.querySelector('#twelve_categories');
@@ -13,62 +12,58 @@
     const cal = document.querySelector('#cal');
     const finalResult = document.querySelector('#result');
 
-    console.log(twentyFourCat, twelveCat, eighteenCat)
     const twentyFourCategories = () => {
         twentyFourCat.addEventListener('change', () => {
-            let twentyFourValue = twentyFourCat.value
-            console.log(twentyFourValue)
-            let remainingPacks = +numberOfItems.value - +wob.value - +goodPack.value - +badPack.value - +notReturnedOrMissing.value;
-            let canConvertion = remainingPacks * 24;
-            let remainingCans = canConvertion - +goodCan.value - +badCan.value
-            let finalPack = Math.floor(remainingCans / 24);
-            let finalCan = remainingCans - finalPack * 24;
-    
-            finalResult.textContent = `wob: ${+wob.value}, packs: ${finalPack}, cans: ${finalCan}`
-    
-            console.log(remainingPacks, canConvertion, badCan, goodCan, remainingCans, finalPack, finalCan)
+            cal.addEventListener('click', () => {
+                let twentyFourValue = twentyFourCat.value
+                console.log(twentyFourValue)
+                let remainingPacks = +numberOfItems.value - +wob.value - +goodPack.value - +badPack.value - +notReturnedOrMissing.value;
+                let canConvertion = remainingPacks * 24;
+                let remainingCans = canConvertion - +goodCan.value - +badCan.value
+                let finalPack = Math.floor(remainingCans / 24);
+                let finalCan = remainingCans - finalPack * 24;
+        
+                finalResult.textContent = `wob: ${+wob.value}, packs: ${finalPack}, cans: ${finalCan}`
+            })
         })
     }
 
     const eighteenCategories = () => {
         eighteenCat.addEventListener('change', () => {
-            let eighteenValue = eighteenCat.value
-            console.log(eighteenValue)
-            let remainingPacks = +numberOfItems.value - +wob.value - +goodPack.value - +badPack.value - +notReturnedOrMissing.value;
-            let canConvertion = remainingPacks * 18;
-            let remainingCans = canConvertion - +goodCan.value - +badCan.value
-            let finalPack = Math.floor(remainingCans / 18);
-            let finalCan = remainingCans - finalPack * 18;
-    
-            finalResult.textContent = `wob: ${+wob.value}, packs: ${finalPack}, cans: ${finalCan}`
-    
-            console.log(remainingPacks, canConvertion, badCan, goodCan, remainingCans, finalPack, finalCan)
+            cal.addEventListener('click', () => {
+                let eighteenValue = eighteenCat.value
+                console.log(eighteenValue)
+                let remainingPacks = +numberOfItems.value - +wob.value - +goodPack.value - +badPack.value - +notReturnedOrMissing.value;
+                let canConvertion = remainingPacks * 18;
+                let remainingCans = canConvertion - +goodCan.value - +badCan.value
+                let finalPack = Math.floor(remainingCans / 18);
+                let finalCan = remainingCans - finalPack * 18;
+        
+                finalResult.textContent = `wob: ${+wob.value}, packs: ${finalPack}, cans: ${finalCan}`;
+            })
         })
     }
 
     const twelveCategories = () => {
         twelveCat.addEventListener('change', () => {
-            let twelveValue = twelveCat.value
-            console.log(twelveValue)
-            let remainingPacks = +numberOfItems.value - +wob.value - +goodPack.value - +badPack.value - +notReturnedOrMissing.value;
-            let canConvertion = remainingPacks * 12;
-            let remainingCans = canConvertion - +goodCan.value - +badCan.value
-            let finalPack = Math.floor(remainingCans / 12);
-            let finalCan = remainingCans - finalPack * 12;
-    
-            finalResult.textContent = `wob: ${+wob.value}, packs: ${finalPack}, cans: ${finalCan}`
-    
-            console.log(remainingPacks, canConvertion, badCan, goodCan, remainingCans, finalPack, finalCan)
+            cal.addEventListener('click', ()=> {
+                let twelveValue = twelveCat.value
+                console.log(twelveValue)
+                let remainingPacks = +numberOfItems.value - +wob.value - +goodPack.value - +badPack.value - +notReturnedOrMissing.value;
+                let canConvertion = remainingPacks * 12;
+                let remainingCans = canConvertion - +goodCan.value - +badCan.value
+                let finalPack = Math.floor(remainingCans / 12);
+                let finalCan = remainingCans - finalPack * 12;
+        
+                finalResult.textContent = `wob: ${+wob.value}, packs: ${finalPack}, cans: ${finalCan}`
+            })
+            
         })
     }
 
-    function text() {
-        twelveCategories()
-        twentyFourCategories()
-        eighteenCategories()
-    }
-
-    cal.addEventListener('click', text)
+    twelveCategories()
+    twentyFourCategories()
+    eighteenCategories()
 
 
 })()
