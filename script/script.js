d8205ecb8a9b832b523e23a2604e2240209bb350
aisdev <aisosamatthew247@gmail.com>
2024-11-05 13:41:28 +0100

    Did the final update, modification will come when need in the future

--- script/script.js
+++ script/script.js
@@ -11,14 +11,33 @@
     const notReturnedOrMissing = document.querySelector('#not_return_or_missing');
     const para = document.querySelectorAll('p');
     const drinksName = document.querySelector('h2');
+    const resultHeadings = document.querySelectorAll('h3');
+    const type = document.querySelector('#type')
     const warning = document.querySelector('.warning')
     const btn = document.querySelectorAll('button')
+
+    type.addEventListener('change', () => {
+        let typeValue = type.value;
+        let typeValueChild = "";
+        if(typeValue === 'crate') {
+            typeValueChild = "bottle";
+            resultHeadings[2].textContent = typeValue;
+            resultHeadings[3].textContent = typeValueChild;
+        } else if(typeValue === 'case') {
+            typeValueChild = "pet";
+            resultHeadings[2].textContent = typeValue;
+            resultHeadings[3].textContent = typeValueChild;
+        } else {
+            typeValueChild = 'can'
+            resultHeadings[2].textContent = typeValue;
+            resultHeadings[3].textContent = typeValueChild;
+        }
+    })
 
     const twentyFourCategories = () => {
         twentyFourCat.addEventListener('change', () => {
             btn[1].addEventListener('click', () => {
                 let twentyFourValue = twentyFourCat.value
-                console.log(twentyFourValue)
                 let remainingPacks = +numberOfItems.value - +wob.value - +goodPack.value - +badPack.value - +notReturnedOrMissing.value;
                 let canConvertion = remainingPacks * 24;
                 let remainingCans = canConvertion - +goodCan.value - +badCan.value
@@ -26,7 +45,7 @@
                 let finalCan = remainingCans - finalPack * 24;
                 let totalCan = (+wob.value * 24) + (finalPack * 24) + (+notReturnedOrMissing.value * 24) + finalCan;
         
-                drinksName.textContent = twentyFourCat.value
+                drinksName.textContent = twentyFourValue;
                 para[3].textContent = +wob.value;
                 para[4].textContent = finalPack;
                 para[5].textContent = finalCan;
@@ -34,11 +53,11 @@
                 para[7].textContent = totalCan;
 
                 if(+wob.value >= 11 || +notReturnedOrMissing.value || finalPack) {
-                    warning.classList.add('warn')
+                    warning.classList.add('warn');
                 }
 
                 btn[0].addEventListener('click', () => {
-                    warning.classList.remove('warn')
+                    warning.classList.remove('warn');
                 })
             })
         })
@@ -48,7 +67,6 @@
         eighteenCat.addEventListener('change', () => {
             btn[1].addEventListener('click', () => {
                 let eighteenValue = eighteenCat.value
-                console.log(eighteenValue)
                 let remainingPacks = +numberOfItems.value - +wob.value - +goodPack.value - +badPack.value - +notReturnedOrMissing.value;
                 let canConvertion = remainingPacks * 18;
                 let remainingCans = canConvertion - +goodCan.value - +badCan.value
@@ -56,7 +74,7 @@
                 let finalCan = remainingCans - finalPack * 18;
                 let totalCan = (+wob.value * 18) + (finalPack * 18) + (+notReturnedOrMissing.value * 18) + finalCan;
         
-                drinksName.textContent = eighteenCat.value
+                drinksName.textContent = eighteenValue;
                 para[3].textContent = +wob.value;
                 para[4].textContent = finalPack;
                 para[5].textContent = finalCan;
@@ -64,11 +82,11 @@
                 para[7].textContent = totalCan;
                 
                 if(+wob.value >= 11 || +notReturnedOrMissing.value || finalPack) {
-                    warning.classList.add('warn')
+                    warning.classList.add('warn');
                 }
 
                 btn[0].addEventListener('click', () => {
-                    warning.classList.remove('warn')
+                    warning.classList.remove('warn');
                 })
             })
         })
@@ -78,7 +96,6 @@
         twelveCat.addEventListener('change', () => {
             btn[1].addEventListener('click', ()=> {
                 let twelveValue = twelveCat.value
-                console.log(twelveValue)
                 let remainingPacks = +numberOfItems.value - +wob.value - +goodPack.value - +badPack.value - +notReturnedOrMissing.value;
                 let canConvertion = remainingPacks * 12;
                 let remainingCans = canConvertion - +goodCan.value - +badCan.value
@@ -86,7 +103,7 @@
                 let finalCan = remainingCans - finalPack * 12;
                 let totalCan = (+wob.value * 12) + (finalPack * 12) + (+notReturnedOrMissing.value * 12) + finalCan;
         
-                drinksName.textContent = twelveCat.value
+                drinksName.textContent = twelveValue;
                 para[3].textContent = +wob.value;
                 para[4].textContent = finalPack;
                 para[5].textContent = finalCan;
